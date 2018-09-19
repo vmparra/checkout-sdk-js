68c2fd9acdaab352df17ad7fd07b5f8a765f5c2c
Irving Gomez <lordzero0000@gmail.com>
2018-09-19 11:02:39 -0500

    feat(payment): INT-838 [NGCheckout] Add Google Pay button to Cart Page

---
+++ src/checkout-buttons/strategies/googlepay/googlepay-braintree-button.mock.ts
@@ -0,0 +1,63 @@
+import { createRequestSender } from '@bigcommerce/request-sender/lib';
+import { getScriptLoader } from '@bigcommerce/script-loader/lib/';
+
+import { BillingAddressActionCreator, BillingAddressRequestSender } from '../../../billing';
+import { CheckoutStore } from '../../../checkout';
+import { PaymentMethod, PaymentMethodActionCreator, PaymentMethodRequestSender } from '../../../payment';
+import { getGooglePay } from '../../../payment/payment-methods.mock';
+import { BraintreeScriptLoader, BraintreeSDKCreator } from '../../../payment/strategies/braintree';
+import { GooglePayBraintreeInitializer, GooglePayPaymentProcessor, GooglePayScriptLoader } from '../../../payment/strategies/googlepay';
+import { CheckoutButtonInitializeOptions } from '../../checkout-button-options';
+
+const requestSender = createRequestSender();
+const scriptLoader = getScriptLoader();
+const braintreeSdkCreator = new BraintreeSDKCreator(new BraintreeScriptLoader(scriptLoader));
+
+export function getGooglePayPaymentProcessor(store: CheckoutStore): GooglePayPaymentProcessor {
+
+    return new GooglePayPaymentProcessor(
+        store,
+        new PaymentMethodActionCreator(new PaymentMethodRequestSender(requestSender)),
+        new GooglePayScriptLoader(scriptLoader),
+        new GooglePayBraintreeInitializer(braintreeSdkCreator),
+        new BillingAddressActionCreator(new BillingAddressRequestSender(requestSender)),
+        requestSender
+    );
+}
+
+export function getPaymentMethod(): PaymentMethod {
+    return {
+        ...getGooglePay(),
+        initializationData: {
+            checkoutId: 'checkoutId',
+            allowedCardTypes: ['visa', 'amex', 'mastercard'],
+        },
+    };
+}
+
+export enum Mode {
+    Full,
+    UndefinedMethodId,
+    InvalidContainer,
+    Incomplete,
+}
+
+export function getCheckoutButtonOptions(mode: Mode = Mode.Full): CheckoutButtonInitializeOptions {
+    const methodId = { methodId: 'googlepay' };
+    const container = { container: 'googlePayCheckoutButton' };
+    const invalidContainer = { container: 'invalid_container' };
+    const googlepay = { googlepaybraintree: { ...container } };
+    const googlepayWithInvalidContainer = { googlepaybraintree: { ...invalidContainer } };
+
+    switch (mode) {
+        case Mode.Incomplete: {
+            return { ...methodId };
+        }
+        case Mode.InvalidContainer: {
+            return { ...methodId, ...googlepayWithInvalidContainer };
+        }
+        default: {
+            return { ...methodId, ...googlepay };
+        }
+    }
+}
