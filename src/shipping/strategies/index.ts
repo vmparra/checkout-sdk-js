45efcdf4955ff358b31cc5e9f7af30d64cc37c39
Victor Parra <victor.parra@bigcommerce.com>
2018-10-05 13:33:35 -0500

    feat(payment): INT-836 Create Customer Strategy

--- src/shipping/strategies/index.ts
+++ src/shipping/strategies/index.ts
@@ -1,3 +1,4 @@
 export { default as AmazonPayShippingStrategy, AmazonPayShippingInitializeOptions } from './amazon-pay-shipping-strategy';
 export { default as DefaultShippingStrategy } from './default-shipping-strategy';
 export { default as ShippingStrategy } from './shipping-strategy';
+export { default as GooglePayBraintreeShippingStrategy } from './googlepay-braintree-shipping-strategy';
